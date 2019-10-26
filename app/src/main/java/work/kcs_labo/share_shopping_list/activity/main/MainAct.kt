14e9049bc7c711e08d1eb33ceb8cc7978a3bfc36
Hide-KC <kcs.dev.labo@gmail.com>
2019-10-26 17:32:54 +0900

    initialize adsence

--- app/src/main/java/work/kcs_labo/share_shopping_list/activity/main/MainAct.kt
+++ app/src/main/java/work/kcs_labo/share_shopping_list/activity/main/MainAct.kt
@@ -1,10 +1,12 @@
 package work.kcs_labo.share_shopping_list.activity.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
+import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
+import kotlinx.android.synthetic.main.main_act.*
 import work.kcs_labo.share_shopping_list.R
 import work.kcs_labo.share_shopping_list.databinding.MainActBinding
 import work.kcs_labo.share_shopping_list.util.ViewModelFactory
@@ -17,11 +19,29 @@ class MainAct : AppCompatActivity() {
     val binding = DataBindingUtil
       .setContentView<MainActBinding>(this, R.layout.main_act)
       .also {
-        it.viewModel = ViewModelProviders.of(this, ViewModelFactory.getInstance(application)).get(MainActViewModel::class.java)
+        it.viewModel = ViewModelProviders.of(this, ViewModelFactory.getInstance(application))
+          .get(MainActViewModel::class.java)
       }
 
-    if (savedInstanceState == null){
-      MobileAds.initialize(this)
+    if (savedInstanceState == null) {
+      MobileAds.initialize(this, getString(R.string.ads_app_id))
     }
+
+    adView.loadAd(AdRequest.Builder().build())
+  }
+
+  override fun onPause() {
+    super.onPause()
+    adView?.pause()
+  }
+
+  override fun onResume() {
+    super.onResume()
+    adView?.resume()
+  }
+
+  override fun onDestroy() {
+    adView?.destroy()
+    super.onDestroy()
   }
 }
